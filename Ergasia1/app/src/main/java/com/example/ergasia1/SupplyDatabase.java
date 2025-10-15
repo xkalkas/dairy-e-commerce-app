5780bdd58a3625795da9d91e404b7a127b24a68b
xkalkas <xkalkas@gmail.com>
2025-10-15 07:32:21 +0300

    Revert "Upload project to github"
    This reverts commit b09cec0704bc9222f28bf0ebd65387d2aefcb28f.

--- Ergasia1/app/src/main/java/com/example/ergasia1/SupplyDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.ergasia1;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Company.class, Product.class, Supply.class}, version = 1)
-public abstract class SupplyDatabase extends RoomDatabase {
-    public abstract MyDao myDao();
-}
